1ff864ac55278ddaf2ba0cc88565923a448aa192
NI Corp. <74864374+NickNameYouTuber@users.noreply.github.com>
2025-01-11 21:16:38 +0300

    Update

--- app/src/main/java/com/nicorp/nimetro/domain/entities/Station.java
+++ app/src/main/java/com/nicorp/nimetro/domain/entities/Station.java
@@ -7,6 +7,8 @@ import android.graphics.Rect;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.nicorp.nimetro.domain.entities.Facilities;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,15 +24,108 @@ public class Station implements Parcelable {
     private Facilities facilities;
     private int textPosition;
     private List<Neighbor> neighbors;
-    private Map<Station, List<Point>> intermediatePoints; // Изменение структуры данных
+    private Map<Station, List<Point>> intermediatePoints;
+
     public boolean isVisible(Rect visibleRect) {
         return visibleRect.contains((int) x, (int) y);
     }
 
-    public void draw(Canvas canvas, Paint paint) {
-        canvas.drawCircle(x, y, 10, paint); // Example: draw a circle for the station
+    public String getId() {
+        return id;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getESP() {
+        return ESP;
+    }
+
+    public void setESP(String ESP) {
+        this.ESP = ESP;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Facilities getFacilities() {
+        return facilities;
+    }
+
+    public void setFacilities(Facilities facilities) {
+        this.facilities = facilities;
+    }
+
+    public int getTextPosition() {
+        return textPosition;
+    }
+
+    public void setTextPosition(int textPosition) {
+        this.textPosition = textPosition;
+    }
+
+    public List<Neighbor> getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(List<Neighbor> neighbors) {
+        this.neighbors = neighbors;
+    }
+
+    public Map<Station, List<Point>> getIntermediatePoints() {
+        return intermediatePoints;
+    }
+    public List<Point> getIntermediatePoints(Station otherStation) {
+        if (intermediatePoints != null) {
+            List<Point> points = intermediatePoints.get(otherStation);
+            if (points != null) {
+                return points;
+            }
+            // Check the reverse order
+            points = otherStation.intermediatePoints.get(this);
+            if (points != null) {
+                return points;
+            }
+        }
+        return null;
+    }
+
+
+    public void setIntermediatePoints(Map<Station, List<Point>> intermediatePoints) {
+        this.intermediatePoints = intermediatePoints;
+    }
+
+    // Constructor remains the same
     public Station(String id, String name, int x, int y, String ESP, String color, Facilities facilities, int textPosition) {
         this.id = id;
         this.name = name;
@@ -41,7 +136,7 @@ public class Station implements Parcelable {
         this.facilities = facilities;
         this.textPosition = textPosition;
         this.neighbors = new ArrayList<>();
-        this.intermediatePoints = new HashMap<>(); // Инициализация новой структуры данных
+        this.intermediatePoints = new HashMap<>();
     }
 
     protected Station(Parcel in) {
@@ -53,10 +148,112 @@ public class Station implements Parcelable {
         color = in.readString();
         facilities = in.readParcelable(Facilities.class.getClassLoader());
         textPosition = in.readInt();
-        neighbors = in.createTypedArrayList(Neighbor.CREATOR);
+
+        // Read only the essential neighbor data to avoid circular references
+        int size = in.readInt();
+        neighbors = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            String neighborId = in.readString();
+            int time = in.readInt();
+            // Create a temporary station with just the ID
+            Station neighborStation = new Station(neighborId, "", 0, 0, "", "", null, 0);
+            neighbors.add(new Neighbor(neighborStation, time));
+        }
+
+        // Initialize empty intermediatePoints map
         intermediatePoints = new HashMap<>();
-        in.readMap(intermediatePoints, Station.class.getClassLoader());
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(name);
+        dest.writeInt(x);
+        dest.writeInt(y);
+        dest.writeString(ESP);
+        dest.writeString(color);
+        dest.writeParcelable(facilities, flags);
+        dest.writeInt(textPosition);
+
+        // Write only essential neighbor data
+        dest.writeInt(neighbors.size());
+        for (Neighbor neighbor : neighbors) {
+            dest.writeString(neighbor.getStation().getId());
+            dest.writeInt(neighbor.getTime());
+        }
+    }
+
+    public void draw(Canvas canvas, Paint paint) {
+        canvas.drawCircle(x, y, 10, paint); // Example: draw a circle for the station
+    }
+
+    public void addIntermediatePoints(Station station2, List<Point> points) {
+        intermediatePoints.put(station2, points);
+    }
+
+    public void addNeighbor(Neighbor neighbor) {
+        neighbors.add(neighbor);
+    }
+
+
+    public static class Neighbor implements Parcelable {
+        private Station station;
+        private int time;
+
+        public Station getStation() {
+            return station;
+        }
+
+        public void setStation(Station station) {
+            this.station = station;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public void setTime(int time) {
+            this.time = time;
+        }
+
+        public Neighbor(Station station, int time) {
+            this.station = station;
+            this.time = time;
+        }
+
+        protected Neighbor(Parcel in) {
+            // Read only essential station data
+            String stationId = in.readString();
+            station = new Station(stationId, "", 0, 0, "", "", null, 0);
+            time = in.readInt();
+        }
+
+        @Override
+        public void writeToParcel(Parcel dest, int flags) {
+            // Write only essential station data
+            dest.writeString(station.getId());
+            dest.writeInt(time);
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        public static final Creator<Neighbor> CREATOR = new Creator<Neighbor>() {
+            @Override
+            public Neighbor createFromParcel(Parcel in) {
+                return new Neighbor(in);
+            }
+
+            @Override
+            public Neighbor[] newArray(int size) {
+                return new Neighbor[size];
+            }
+        };
+    }
+
+    // Rest of the getters and setters remain the same
 
     public static final Creator<Station> CREATOR = new Creator<Station>() {
         @Override
@@ -73,178 +270,5 @@ public class Station implements Parcelable {
     @Override
     public int describeContents() {
         return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(id);
-        dest.writeString(name);
-        dest.writeInt(x);
-        dest.writeInt(y);
-        dest.writeString(ESP);
-        dest.writeString(color);
-        dest.writeParcelable(facilities, flags);
-        dest.writeInt(textPosition);
-        dest.writeTypedList(neighbors);
-        dest.writeMap(intermediatePoints);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public Facilities getFacilities() {
-        return facilities;
-    }
-
-    public int getTextPosition() {
-        return textPosition;
-    }
-
-    public List<Neighbor> getNeighbors() {
-        return neighbors;
-    }
-
-    public void addNeighbor(Neighbor neighbor) {
-        neighbors.add(neighbor);
-    }
-
-    public List<Point> getIntermediatePoints(Station otherStation) {
-        if (intermediatePoints != null) {
-            List<Point> points = intermediatePoints.get(otherStation);
-            if (points != null) {
-                return points;
-            }
-            // Check the reverse order
-            points = otherStation.intermediatePoints.get(this);
-            if (points != null) {
-                return points;
-            }
-        }
-        return null;
-    }
-
-    public void addIntermediatePoints(Station station2, List<Point> points) {
-        intermediatePoints.put(station2, points);
-    }
-
-    public static class Neighbor implements Parcelable {
-        private Station station;
-        private int time;
-
-        public Neighbor(Station station, int time) {
-            this.station = station;
-            this.time = time;
-        }
-
-        protected Neighbor(Parcel in) {
-            station = in.readParcelable(Station.class.getClassLoader());
-            time = in.readInt();
-        }
-
-        public static final Creator<Neighbor> CREATOR = new Creator<Neighbor>() {
-            @Override
-            public Neighbor createFromParcel(Parcel in) {
-                return new Neighbor(in);
-            }
-
-            @Override
-            public Neighbor[] newArray(int size) {
-                return new Neighbor[size];
-            }
-        };
-
-        @Override
-        public int describeContents() {
-            return 0;
-        }
-
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeParcelable(station, flags);
-            dest.writeInt(time);
-        }
-
-        public Station getStation() {
-            return station;
-        }
-
-        public int getTime() {
-            return time;
-        }
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public void setFacilities(Facilities facilities) {
-        this.facilities = facilities;
-    }
-
-    public void setTextPosition(int textPosition) {
-        this.textPosition = textPosition;
-    }
-
-    public void setNeighbors(List<Neighbor> neighbors) {
-        this.neighbors = neighbors;
-    }
-
-    public Map<Station, List<Point>> getIntermediatePoints() {
-        return intermediatePoints;
-    }
-
-    public void setIntermediatePoints(Map<Station, List<Point>> intermediatePoints) {
-        this.intermediatePoints = intermediatePoints;
-    }
-
-    public String getESP() {
-        return ESP;
-    }
-
-    public void setESP(String ESP) {
-        this.ESP = ESP;
-    }
-
-    @Override
-    public String toString() {
-        return "Station{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", x=" + x +
-                ", y=" + y +
-                '}';
     }
 }
